c6ddfbb9a2960741dc0e6472db434d745f3c5096
Agusstin Martilotta <agustin.martilotta@hotmail.com>
2024-10-31 04:42:36 +0000

    Modify Schemas

--- src/schemas/task_schema.py
+++ src/schemas/task_schema.py
@@ -1,19 +1,29 @@
+from typing import List, Optional
+
 from pydantic import BaseModel
 
 
-class TaskCreate(BaseModel):
+class TaskCreateSchema(BaseModel):
     title: str
     description: str
 
 
-class TaskResponse(TaskCreate):
-    id: int
+class TaskUpdateSchema(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
     completed: bool = False
 
-    class Config:
-        orm_mode = True
+
+class TaskResponse(TaskCreateSchema):
+    id: int
+    completed: bool = False
 
 
 class TaskOutput(BaseModel):
     message: str
     task: TaskResponse
+
+
+class TaskGetOutput(BaseModel):
+    message: str
+    tasks: List[TaskResponse]
