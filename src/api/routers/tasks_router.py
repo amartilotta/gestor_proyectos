f648d28b4a58a4f98fbee887c43a839099db0d21
Agusstin Martilotta <agustin.martilotta@hotmail.com>
2024-11-01 12:16:57 +0000

    Fix linter

--- src/api/routers/tasks_router.py
+++ src/api/routers/tasks_router.py
@@ -1,14 +1,20 @@
 from controller.task_controller import TaskController
 from fastapi import APIRouter
-
 from schemas.task_schema import TaskOutput
 
 router = APIRouter()
 
 
-router.add_api_route("/tasks/", TaskController.create_task, methods=["POST"],responses={
-    200:{"model":TaskOutput}  # TODO: Make this in all of ones and do the schemas
-})
+router.add_api_route(
+    "/tasks/",
+    TaskController.create_task,
+    methods=["POST"],
+    responses={
+        200: {
+            "model": TaskOutput
+        }  # TODO: Make this in all of ones and do the schemas
+    },
+)
 router.add_api_route("/tasks/", TaskController.get_task, methods=["GET"])
 router.add_api_route(
     "/tasks/{id}", TaskController.get_task_by_id, methods=["GET"]
